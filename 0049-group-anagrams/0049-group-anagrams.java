f47d57c585baa596e100bd626c484a66957c7882
Pranjal Raghuvanshi <118126928+Pranjal2506@users.noreply.github.com>
2024-09-24 15:04:44 +0530

    Time: 34 ms (9.17%), Space: 49.3 MB (6.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>>  li = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
         HashMap<String, List<String>> map = new HashMap<>();
         for(String str: strs){
-            char[] char_str = str.toCharArray();
-            Arrays.sort(char_str);
-            String fin_str = new String(char_str);
-            if(!map.containsKey(fin_str)){
-                map.put(fin_str, new ArrayList<>());
+            String key = findKey(str);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
             }
-            map.get(fin_str).add(str);
+            map.get(key).add(str);
         }
-        for(List<String> vals: map.values()){
-            li.add(vals);
+        for(String s: map.keySet()){
+            ans.add(map.get(s));
         }
-        return li;
+        return ans;
+    }
+    public String findKey(String str){
+        int[] arr = new int[26];
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            arr[ch-'a'] = arr[ch-'a'] + 1;
+        }
+        String st = "";
+        for(int i=0; i<arr.length; i++){
+            st = st+ arr[i]+",";
+        }
+        return st;
     }
 }
