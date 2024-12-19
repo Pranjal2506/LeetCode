75b20d1a54c02a6b9d18eb1bd2986e7719639919
Pranjal Raghuvanshi <118126928+Pranjal2506@users.noreply.github.com>
2024-12-20 02:05:25 +0530

    Time: 39 ms (11.33%), Space: 45 MB (76.99%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,33 +1,19 @@
-import java.util.*;
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        HashSet<Integer>set = new HashSet<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(map1.containsKey(ch)){
-                map1.put(ch, map1.get(ch)+1);
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch)+1);
             }else{
-                map1.put(ch, 1);
+                map.put(ch, 1);
             }
         }
         for(int i=0; i<s.length(); i++){
-            char ch2 = s.charAt(i);
-            map2.put(ch2, i);
-        }
-        for(Character ch: map1.keySet()){
-            if(map1.get(ch) != 1){
-                map2.remove(ch);
+            if(map.get(s.charAt(i)) == 1){
+                return i;
             }
         }
-        Collection<Integer> values = map2.values();
-        if(!values.isEmpty()){
-            Integer min = Collections.min(values);
-            if(min >= 0){
-                return min;
-        }
+        return -1;
     }
-    return -1;
-}
 }
